f0d8b01488b935076b6a9a6315e28572c4d8b522
Armaan Shah <armaanshah96@gmail.com>
2017-04-26 08:00:20 -0500

    initial commit w simple express app

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var engines = require('consolidate');
+var path = require('path');
+
+var app = new express();
+
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.set('port', 3000);
+
+app.get('*', function(request, response) {
+  response.render('index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express app listening on port ' + app.get('port'));
+});
