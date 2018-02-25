3014a1e58b0879517131eedd354ee55857d4146e
Maggie <margaretlou2019@u.northwestern.edu>
2018-02-25 16:30:27 -0600

    Restructure to use Jade

--- server.js
+++ server.js
@@ -2,34 +2,30 @@ var express = require('express');
 var engines = require('consolidate');
 var path = require('path');
 var bodyParser = require('body-parser');
-
-// var Groups = require('./models/groups');
 var mongoose = require('mongoose');
+var Env = require('dotenv');
+var fs = require('fs');
+
 var mongoDB = 'mongodb://localhost/db';
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Database connection error:'));
 
-var Env = require('dotenv');
-var fs = require('fs');
-//var mustache = require('express-mustache');
-
+// Set Routes
 var GroupController = require('./controllers/GroupController');
-
 var UserController = require('./controllers/UserController');
-
+var Test = require('./controllers/Test');
 
 Env.load();
-
 var app = new express();
 
-app.engine('html', engines.mustache);
-//app.engine('html', mustacheExpress());
-// Static files are accessible from both public and node_modules directories
+// View engine setup
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+  // Static files are accessible from both public and node_modules directories
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
-app.set('view engine', 'html');
-//app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+
 var port = process.env.PORT || 3000;
 app.set('port', port);
 
@@ -40,9 +36,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/groups', GroupController);
-
 app.use('/users', UserController);
-
+app.use('/test', Test);
 
 app.get('/', function(request, response) {
   response.render('index.html');
